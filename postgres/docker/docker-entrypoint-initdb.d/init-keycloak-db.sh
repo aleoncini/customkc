b7008dc036ec6baf0bf51ed09d9717ca68d700f9
Andrea Leoncini <andrea.leoncini@gmail.com>
2023-01-13 15:10:34 +0100

    reorganazing folders

--- postgres/docker/docker-entrypoint-initdb.d/init-keycloak-db.sh
+++ postgres/docker/docker-entrypoint-initdb.d/init-keycloak-db.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "admin" --dbname "keycloak" <<-EOSQL
-	CREATE USER keycloak WITH PASSWORD 'password';
-	CREATE DATABASE keycloak;
-	GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
-EOSQL
+CREATE USER keycloak WITH PASSWORD password;
+CREATE DATABASE keycloak;
+GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
